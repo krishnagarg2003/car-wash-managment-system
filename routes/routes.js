729f93a7af27449105c6d5c5a6b276ae518d0edf
krishna garg <gargkrishna987070@gmail.com>
2025-03-21 22:41:24 +0530

    service and home page

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,18 @@
 import express from 'express';
 const router = express.Router();
+// router.get("/", async (req, res) => {  
+//     res.render('home', { title: 'Login Page' });
+// }
 router.get("/", async (req, res) => {
- 
-   
-    res.render('home', { title: 'Login Page' });
+    res.render('home', { title: 'Home Page' });
+});
 
-  
-}
+// Services page route
+router.get('/services', (req, res) => {
+    res.render('services', { title: 'Our Services' });
+});
 
-
-);
+// );
 // router.get("/Register", async (req, res) => {
  
    
